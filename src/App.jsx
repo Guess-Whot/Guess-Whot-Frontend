1f9df6e46b7fb5160bd028989cc812da0a620ad9
BrendenSeifried <brendenseifried@gmail.com>
2022-07-13 12:46:56 -0700

    Checking for andys connections this works on my end

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import io from 'socket.io-client';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const socket = io.connect('http://localhost:7890');
+  const sendMessage = () => {
+    // socket.emit('send_message', { message, room });
+  };
+
+  return (
+    <div className="App">
+      <input placeholder="Room Number..." />
+      <button onClick={joinRoom}> Join Room</button>
+      <input placeholder="Message..." />
+      <button onClick={sendMessage}> Send Message</button>
+    </div>
+  );
 }

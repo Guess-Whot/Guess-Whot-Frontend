52d5800c5d363e3076e5bb1eb261638a7829de71
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-07-13 14:39:30 -0700

    added soket io to app.js with rooms

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
-export default function App() {
-  return <h1>Hello World</h1>;
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:7890');
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [received, setReceived] = useState('');
+  const [room, setRoom] = useState('');
+
+  const sendMessage = () => {
+    socket.emit('send_message', { message, room });
+  };
+
+  const joinRoom = () => {
+    if (room !== '') {
+      socket.emit('join_room', room);
+    }
+  };
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setReceived(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div className="App">
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Message:</h1>
+      {received}
+    </div>
+  );
 }
+
+export default App;

b229d2dfbf4deec4b4596feaeab291f7c2cd2032
Alex Orlet <AlexOrlet89@gmail.com>
2022-07-13 17:16:29 -0700

    App Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute exact path="/">
+            <Landing />
+          </PrivateRoute>
+          <Route exact path="/auth">
+            <Auth />
+          </Route>
+          <PrivateRoute exact path="/lobby">
+            <Lobby />
+          </PrivateRoute>
+          <PrivateRoute exact path="/game">
+            <Lobby />
+          </PrivateRoute>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }

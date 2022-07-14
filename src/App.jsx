3b87edf9dcf7b2cfa74e81b52c60682918770d6c
Alex Orlet <AlexOrlet89@gmail.com>
2022-07-13 17:46:31 -0700

    fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 export default function App() {
   return (

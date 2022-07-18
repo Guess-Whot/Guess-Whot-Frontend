595dd4928cc5ffc59020e61e77ca735b649d0842
BrendenSeifried <brendenseifried@gmail.com>
2022-07-18 09:53:33 -0700

    removed SRC to catch up with current code

--- src/services/chars.js
+++
@@ -1,9 +0,0 @@
-export async function fetchChar() {
-  try {
-    const data = await fetch(`${process.env.BACKEND_URL}/api/v1/characters`);
-    const resp = await data.json();
-    return resp;
-  } catch (error) {
-    console.log(error);
-  }
-}
--- src/views/Lobby.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ChatRooms from '../components/RoomChat/roomChat';
-export default function Lobby() {
-  return (
-    <>
-      <ChatRooms />
-    </>
-  );
-}

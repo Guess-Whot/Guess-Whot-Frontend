6601a35f8181fbd52047c9585a26cec9af9999d5
BrendenSeifried <brendenseifried@gmail.com>
2022-07-20 10:45:26 -0700

    new route /test made for secret char selector

--- src/components/CharCard.jsx
+++ src/components/CharCard.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import io from 'socket.io-client';
 import useRoomChat from '../hooks/useRoomChat';
-import { CardContainer, StyledCard, StyledCardBack, StyledCardInner } from './Styles/StyledCard';
+import {
+  CardContainer,
+  StyledCard,
+  StyledCardBack,
+  StyledCardInner,
+} from './Styles/StyledCard';
 
 // const socket = io.connect('http://localhost:7890');
 // const socket = io.connect(`${process.env.BACKEND_URL}:7890`);
 
 export default function Character({ id, name, url }) {
-
   const { flipHandlerBackend } = useRoomChat();
   const [flipped, setFlipped] = useState(false);
 
@@ -22,19 +26,18 @@ export default function Character({ id, name, url }) {
   return (
     <CardContainer>
       <StyledCardInner className={flipped ? 'flipped' : ''}>
-        <StyledCard
-          onClick={() => flipHandler()}>
-            <img src= {url}
-            alt={`image of ${name} `} />
+        <StyledCard onClick={() => flipHandler()}>
+          <img src={url} alt={`image of ${name} `} />
           <h3>{name}</h3>
         </StyledCard>
 
-        <StyledCardBack
-          onClick={() => flipHandler()}>
-            <img src= 'https://static.wikia.nocookie.net/enfuturama/images/1/13/Planet_express.png'
-            alt={`image of ${name} `} />
+        <StyledCardBack onClick={() => flipHandler()}>
+          <img
+            src="https://static.wikia.nocookie.net/enfuturama/images/1/13/Planet_express.png"
+            alt={`image of ${name} `}
+          />
           <h3>{name}</h3>
-        </StyledCardBack> 
+        </StyledCardBack>
       </StyledCardInner>
     </CardContainer>
   );

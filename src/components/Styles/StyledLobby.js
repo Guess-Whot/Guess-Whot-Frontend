e4ecaa25673f3fef0287e7568de2297789f8584a
BrendenSeifried <brendenseifried@gmail.com>
2022-07-22 09:49:34 -0700

    meet the  devs/lobby styalized

--- src/components/Styles/StyledLobby.js
+++ src/components/Styles/StyledLobby.js
@@ -5,7 +5,31 @@ export const StyledLobby = styled.div`
   flex-direction: row;
   flex-wrap: wrap;
   text-align: center;
-  .devCard {
-    border: solid black 5px;
+
+  color: white;
+  img {
+    cursor: crosshair;
+  }
+`;
+export const StyledParent = styled.div`
+  background: blue;
+  height: 100%;
+
+  color: white;
+`;
+export const StyledHeader = styled.div`
+  text-align: center;
+  justify-content: center;
+  align-item: center;
+  h1 {
+    font-size: 8rem;
+  }
+  h3 {
+    margin-left: 45.5%;
+    border: 5px solid black;
+    width: 200px;
+    border-radius: 20px;
+    background: black;
+    cursor: crosshair;
   }
 `;
--- src/views/Lobby.jsx
+++ src/views/Lobby.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { StyledLobby } from '../components/Styles/StyledLobby';
+import {
+  StyledHeader,
+  StyledLobby,
+  StyledParent,
+} from '../components/Styles/StyledLobby';
 import { useSinglePageContext } from '../context/SinglePageContext';
 import useRoomChat from '../hooks/useRoomChat';
 
@@ -15,37 +19,55 @@ export default function Lobby() {
 
   return (
     <>
-      <h1>Meet the Whots</h1>
-      <h3 onClick={() => history.push('/home')}>Proceed to Lobby</h3>
+      <StyledParent>
+        <StyledHeader>
+          <h1>Meet the Whots</h1>
+          <h3 onClick={() => history.push('/home')}>Proceed to Lobby</h3>
+        </StyledHeader>
+        <StyledLobby>
+          <div className="devCard">
+            <a href="https://www.linkedin.com/in/alexorlet89/">
+              <img src="https://i.imgur.com/Jz9kSxF.png" />
+            </a>
+            <h3>Alex Orlet Full-Stack Software Engineer. </h3>
+            <p>He enjoys playing games and being a friend to all.</p>
+          </div>
+          <div className="devCard">
+            <a
+              href="http://www.linkedin.com/in/Andy-Mascaro
+"
+            >
+              <img src="https://i.imgur.com/RncQneJ.png" />
+            </a>
+            <h3>Andy Mascaro</h3>
+            <p>
+              Full-Stack Software Engineer. I enjoy spending time at the beach
+              with my wife and kids.
+            </p>
+          </div>
 
-      <StyledLobby>
-        <div className="devCard">
-          <img src="https://i.imgur.com/Jz9kSxF.png" />
-          <h3>Alex Orlet Full-Stack Software Engineer. </h3>
-          <p>He enjoys playing games and being a friend to all.</p>
-        </div>
-        <div className="devCard">
-          <img src="https://i.imgur.com/RncQneJ.png" />
-          <h3>Andy Mascaro</h3>
-          <p>
-            Full-Stack Software Engineer. I enjoy spending time at the beach
-            with my wife and kids.
-          </p>
-        </div>
-        <div className="devCard">
-          <img src="https://i.imgur.com/RHOt8mV.png" />
-          <h3>Brenden Seifried</h3>
-          <p>Full-Stack Engineer. Loves bad movies and animals.</p>
-        </div>
-        <div className="devCard">
-          <img src="https://i.imgur.com/cHojgpK.png" />
-          <h3>Joshua Stresing</h3>
-          <p>
-            Full-Stack Engineer. Competitive gamer and snack aficionado and also
-            likes to read.
-          </p>
-        </div>
-      </StyledLobby>
+          <div className="devCard">
+            <a
+              href="https://www.linkedin.com/in/brendenseifried/
+"
+            >
+              <img src="https://i.imgur.com/RHOt8mV.png" />
+            </a>
+            <h3>Brenden Seifried</h3>
+            <p>Full-Stack Engineer. Loves bad movies and animals.</p>
+          </div>
+          <div className="devCard">
+            <a href="https://www.linkedin.com/in/joshua-stresing/">
+              <img src="https://i.imgur.com/cHojgpK.png" />
+            </a>
+            <h3>Joshua Stresing</h3>
+            <p>
+              Full-Stack Engineer. Competitive gamer and snack aficionado and
+              also likes to read.
+            </p>
+          </div>
+        </StyledLobby>
+      </StyledParent>
     </>
   );
 }

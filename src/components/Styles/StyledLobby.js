bd75bdb67113a457fd91e1da568ac8c82a1ac5ed
Alex Orlet <AlexOrlet89@gmail.com>
2022-07-22 09:28:28 -0700

    some styling for devCards

--- src/components/Styles/StyledLobby.js
+++ src/components/Styles/StyledLobby.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+export const StyledLobby = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  text-align: center;
+  .devCard {
+    border: solid black 5px;
+  }
+`;
--- src/views/Lobby.jsx
+++ src/views/Lobby.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { StyledLobby } from '../components/Styles/StyledLobby';
 import { useSinglePageContext } from '../context/SinglePageContext';
 import useRoomChat from '../hooks/useRoomChat';
 
@@ -17,22 +18,34 @@ export default function Lobby() {
       <h1>Meet the Whots</h1>
       <h3 onClick={() => history.push('/home')}>Proceed to Lobby</h3>
 
-      <div>
-        <h3>Alex Orlet Full-Stack Software Engineer. </h3>
-        <p>He enjoys playing games and being a friend to all.</p>
-        <h3>Andy Mascaro</h3>
-        <p>
-          Full-Stack Software Engineer. I enjoy spending time at the beach with
-          my wife and kids.
-        </p>
-        <h3>Brenden Seifried</h3>
-        <p>Full-Stack Engineer. Loves bad movies and animals.</p>
-        <h3>Joshua Stresing</h3>
-        <p>
-          Full-Stack Engineer. Competitive gamer and snack aficionado and also
-          likes to read.
-        </p>
-      </div>
+      <StyledLobby>
+        <div className="devCard">
+          <img src="https://i.imgur.com/Jz9kSxF.png" />
+          <h3>Alex Orlet Full-Stack Software Engineer. </h3>
+          <p>He enjoys playing games and being a friend to all.</p>
+        </div>
+        <div className="devCard">
+          <img src="https://i.imgur.com/RncQneJ.png" />
+          <h3>Andy Mascaro</h3>
+          <p>
+            Full-Stack Software Engineer. I enjoy spending time at the beach
+            with my wife and kids.
+          </p>
+        </div>
+        <div className="devCard">
+          <img src="https://i.imgur.com/RHOt8mV.png" />
+          <h3>Brenden Seifried</h3>
+          <p>Full-Stack Engineer. Loves bad movies and animals.</p>
+        </div>
+        <div className="devCard">
+          <img src="https://i.imgur.com/cHojgpK.png" />
+          <h3>Joshua Stresing</h3>
+          <p>
+            Full-Stack Engineer. Competitive gamer and snack aficionado and also
+            likes to read.
+          </p>
+        </div>
+      </StyledLobby>
     </>
   );
 }
